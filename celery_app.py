df610038786117fbddcd6f3b55e418103506bfda
Nandy.py <parnapalli2004@gmail.com>
2026-07-08 17:09:36 +0530

    added celery beat

--- celery_app.py
+++ celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery('tasks', broker='redis://localhost:6379/0')
 
@@ -14,3 +15,18 @@ def long_running_task():
         print(f"Processing step {i + 1}/10...")
         time.sleep(1)  # Simulate a long-running task
     return "Task completed!"
+
+from gchat_webhook import main
+
+@app.task()
+def greet_gspace_ursers():
+    main(msg="Hello from Celery task! This messsage is sheduled for this time....")
+    return "Greeting sent to Google Chat space!"
+
+
+app.conf.beat_schedule = {
+    'greet-gspace-users': {
+        'task': 'celery_app.greet_gspace_ursers',
+        'schedule': crontab(minute=8, hour=17, day_of_month=8)
+    }
+}
